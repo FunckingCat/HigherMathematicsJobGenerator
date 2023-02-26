35dad321b35e0ea3433570cd3091b40840c1d077
Emil-Ka <emilkaraev55@gmail.com>
2023-02-26 11:19:44 +0300

    created login page and removed qr on mobile

---
+++ app/src/pages/login/login.tsx
@@ -0,0 +1,10 @@
+import { type FC } from 'react';
+
+import { Page } from 'widgets';
+import { StudentForm } from 'modules';
+
+export const LoginPage: FC = () => (
+  <Page>
+    <StudentForm />
+  </Page>
+);
