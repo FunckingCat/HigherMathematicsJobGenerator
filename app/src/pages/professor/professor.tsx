4dce38ec951db96a7c455973b93e36c606e2f48d
zavarts <zavoiskih.artyom@yandex.ru>
2023-03-05 22:41:23 +0300

    Did focus on selected difficulty

--- app/src/pages/professor/professor.tsx
+++ app/src/pages/professor/professor.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import { type FC, useEffect, useRef, useState } from 'react';
 
 import { Page } from 'widgets';
 import { PROFESSOR_CONFIG } from 'config';
@@ -6,6 +6,7 @@ import { PROFESSOR_CONFIG } from 'config';
 import { BlockMath } from 'react-katex';
 import 'katex/dist/katex.min.css';
 
+import { type ITaskProps } from './types';
 import styles from './professor.module.scss';
 
 export const ProfessorPage: FC = () => (
@@ -25,16 +26,7 @@ export const ProfessorPage: FC = () => (
             ))}
           </div>
           <div className={styles.tasks}>
-            {section.tasks.map((task) => (
-              <div key={task.id} className={styles.taskContainer}>
-                <BlockMath>{task.task}</BlockMath>
-                <label htmlFor="active" className={styles.difficult}>
-                  <input id="active" type="checkbox" />
-                  <p>{task.difficult}</p>
-                </label>
-                <input type="number" />
-              </div>
-            ))}
+            {section.tasks.map((task) => <Task key={task.id} task={task} />)}
           </div>
         </div>
         <hr />
@@ -42,3 +34,29 @@ export const ProfessorPage: FC = () => (
     ))}
   </Page>
 );
+
+const Task: FC<ITaskProps> = ({ task }) => {
+  const [disabled, setDisabled] = useState(true);
+  const countRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = () => {
+    setDisabled((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (!disabled) {
+      countRef.current?.focus();
+    }
+  }, [disabled]);
+
+  return (
+    <div className={styles.taskContainer}>
+      <BlockMath>{task.task}</BlockMath>
+      <label htmlFor={task.task} className={styles.difficult}>
+        <input id={task.task} type="checkbox" onChange={handleChange} />
+        <p>{task.difficult}</p>
+      </label>
+      <input type="number" disabled={disabled} ref={countRef} />
+    </div>
+  );
+};
