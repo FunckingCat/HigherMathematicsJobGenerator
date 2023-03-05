08c1691204359c52888159bdc46ef83cdbb2137d
AndrewwKovv <kovshov.K@yandex.ru>
2023-03-05 16:20:09 +0300

    added inpu

--- app/src/pages/student/student.tsx
+++ app/src/pages/student/student.tsx
@@ -1,9 +1,21 @@
-import { type FC } from 'react';
-
+import { Button } from 'antd';
+import { type FC, useState } from 'react';
 import { Page } from 'widgets';
 
-export const StudentPage: FC = () => (
-  <Page>
-    Страница студента
+import styles from './student.module.scss';
+
+export const StudentPage: FC = () => {
+  const [username, setUsername] = useState('')
+  const [hash, setHash] = useState('')
+
+  return <Page className={styles.wrapper}>
+    <p>    
+      Введите пожалуйста свою фамилию, имя и группу; код преподавателя
+    </p>
+    <div className={styles.sectionValue}>
+      <input type='text' onChange={(e) => setUsername(e.target.value)} value={username} placeholder='Иванов Иван 211-321'/>
+      <input type='text' onChange={(e) => setHash(e.target.value)} value={hash} placeholder='************'/>
+      <Button type="primary" className={styles.button} onClick={() => console.log('Button work')}>Создать</Button>
+    </div>
   </Page>
-);
+}
