dbd569c4e0e820b12eeff12393c031dfa79b243c
zavarts <zavoiskih.artyom@yandex.ru>
2023-03-12 19:08:53 +0300

    Refactored student page

---
+++ app/src/pages/student/option/constants.ts
@@ -0,0 +1,16 @@
+export const OPTION = {
+  user: 'Иванов Иван Иванович',
+  optionCode: 'Jhbsr234hrbfAJDXNCBkjwh43bJSDB==',
+  tasks: [
+    {
+      id: 5,
+      amount: 1
+    },
+    {
+      id: 2,
+      amount: 1
+    }
+  ],
+  userHash: '309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86' +
+    'd4cd86f4e0b87b20eabe88305d65fd0ed5d93b81de5b5e76d03dcc48e5286e044b7f3'
+};
--- app/src/pages/student/student.tsx
+++ app/src/pages/student/student.tsx
@@ -1,44 +1,13 @@
-import { Button } from 'antd';
-import { type FC, useState, type MouseEvent } from 'react';
+import { type FC } from 'react';
 import { Page } from 'widgets';
-import { useDispatch } from 'react-redux';
-import { studentActions } from 'store/student/slice';
 
 import styles from './student.module.scss';
+import { OptionParameters } from './option-parameters';
+import { Option } from './option';
 
-export const StudentPage: FC = () => {
-  const [name, setUsername] = useState('');
-  const [hash, setHash] = useState('');
-
-  const dispatch = useDispatch();
-
-  const handleClick = (e: MouseEvent<HTMLElement>) => {
-    dispatch(studentActions.addInfo({
-      hash,
-      name
-    }));
-  };
-
-  return (
-    <Page className={styles.wrapper}>
-      <p>
-        Введите пожалуйста свою фамилию, имя и группу; код преподавателя
-      </p>
-      <div className={styles.sectionValue}>
-        <input
-          type="text"
-          onChange={(e) => { setUsername(e.target.value); }}
-          value={name}
-          placeholder="Иванов Иван 211-321"
-        />
-        <input
-          type="text"
-          onChange={(e) => { setHash(e.target.value); }}
-          value={hash}
-          placeholder="************"
-        />
-        <Button type="primary" className={styles.button} onClick={handleClick}>Создать</Button>
-      </div>
-    </Page>
-  );
-};
+export const StudentPage: FC = () => (
+  <Page className={styles.wrapper}>
+    <OptionParameters />
+    <Option />
+  </Page>
+);
