7c47c445f1dbe21b738fd04ca228b160d2eeceb6
zavarts <zavoiskih.artyom@yandex.ru>
2023-03-12 20:06:10 +0300

    Pasted mocks

--- app/src/pages/student/option/constants.ts
+++ app/src/pages/student/option/constants.ts
@@ -1,5 +1,5 @@
 export const OPTION = {
-  user: 'Иванов Иван Иванович',
+  user: 'Иванов Иван 211-321',
   optionCode: 'Jhbsr234hrbfAJDXNCBkjwh43bJSDB==',
   tasks: [
     {
@@ -13,4 +13,31 @@ export const OPTION = {
   ],
   userHash: '309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86' +
     'd4cd86f4e0b87b20eabe88305d65fd0ed5d93b81de5b5e76d03dcc48e5286e044b7f3'
+};
+
+export const TASKS = [
+  {
+    id: 5,
+    name: 'Задание 5',
+    template: 'Шаблон задания 5'
+  },
+  {
+    id: 2,
+    name: 'Задание 2',
+    template: '[cos,sin] + [kx[power]]'
+  }
+];
+
+export const templatesConfig = {
+  bounds: {
+    const_min: -10,
+    const_max: 10,
+    power_max: 4
+  },
+  replacements: {
+    cos: 'cosx',
+    sin: '[odd] * sin([even] * x)',
+    kx: '[const] * x',
+    power: '^[const]'
+  }
 };
--- app/src/pages/student/option/option.tsx
+++ app/src/pages/student/option/option.tsx
@@ -1,3 +1,15 @@
 import { type FC } from 'react';
+import { OPTION } from './constants';
 
-export const Option: FC = () => <div />;
+export const Option: FC = () => (
+  <>
+    <p>
+      Пользователь:
+      {OPTION.user}
+    </p>
+    <p>
+      Код варианта:
+      {OPTION.optionCode}
+    </p>
+  </>
+);
