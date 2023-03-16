7d60125a80e388a79287905e42237aef152997dd
Mikhail Sergeev <sergeevm100@gmail.com>
2023-03-16 22:45:31 +0300

    created professor slice

---
+++ app/src/store/professor/index.ts
@@ -0,0 +1,1 @@
+export { professorActions, professorReducer } from './slice';
---
+++ app/src/store/professor/types.ts
@@ -0,0 +1,8 @@
+export interface IProfessorState {
+  section: string | null
+  values: number[] | null
+}
+
+export type IProfessorSection = Pick<IProfessorState, 'section'>
+export type IProfessorValues = Pick<IProfessorState, 'values'>
+  
