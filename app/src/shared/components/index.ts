e0ae257546cf76a14884d2a9541c91a42a057682
Emil-Ka <emilkaraev55@gmail.com>
2023-04-01 19:22:08 +0300

    added examples with derivatives and the ability to write mathematical expressions in the description of the example

--- app/src/shared/components/index.ts
+++ app/src/shared/components/index.ts
@@ -2,3 +2,4 @@ export { Logo } from './logo';
 export { Container } from './container';
 export { ButtonLink } from './button-link';
 export { Button } from './button';
+export { MathText } from './math-text';
